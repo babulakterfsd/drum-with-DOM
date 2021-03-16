da9ed0b0edd6dc80ec711e959b86c316f352128c
babulakterfsd <babulakterfsd@gmail.com>
2021-03-16 14:07:13 +0600

    settimeout added to minimize the code

--- js/main.js
+++ js/main.js
@@ -8,11 +8,7 @@ window.addEventListener('keydown', function(e) {
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-  }); 
-
-
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', function(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }));
+    setTimeout(() => {
+        key.classList.remove(`playing`)
+    }, 100);
+  });
